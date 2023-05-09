25307286a6f7d5efa3ffec77e03362c62ee15d44
NORDIN M'HAMED <sewasy678@gmail.com>
2023-05-09 13:11:48 +0200

    acabado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
-let counter = 0;
+
+
+    let counter = 0;
 
 $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
+
+    $(".prevBtn").click(function(){
+        counter--;
+        updatecolors();
+    })
+    $(".nextBtn").click(function(){
+        counter++;
+        updatecolors();
+    })
+    
+
+function updatecolors(){
+    $("#counter").text(counter);
+
+    if (counter == 0) {
+        $('#counter').removeClass('positivo');
+        $('#counter').removeClass('negativo');
+    }else if (counter < 0){
+        $("#counter").addClass('negativo');
+    }else if (counter > 0) {
+        $("#counter").addClass('positivo');
+    };
+};
 
 });
